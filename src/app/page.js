73bda8e335215c61e873871b601b2c476bd00214
Mukesh Kumar <83938329+MsRajputana6688@users.noreply.github.com>
2024-01-18 09:12:58 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -58,14 +58,14 @@ const Page = () => {
 
       {
         isLoad ? <Loader /> : <Container>
-          <Row className='py-5'>
-            <Col xxl={6} xl={6} lg={12} md={12} sm={12} xs={12}>
-              <img src="/assets/logo.png" alt="" />
-            </Col>
-            <Col xxl={6} xl={6} lg={12} md={12} sm={12} xs={12}>
-              <img src="/assets/logo-01.png" alt="" />
-            </Col>
-          </Row>
+          // <Row className='py-5'>
+          //   <Col xxl={6} xl={6} lg={12} md={12} sm={12} xs={12}>
+          //     <img src="/assets/logo.png" alt="" />
+          //   </Col>
+          //   <Col xxl={6} xl={6} lg={12} md={12} sm={12} xs={12}>
+          //     <img src="/assets/logo-01.png" alt="" />
+          //   </Col>
+          // </Row>
 
           <Row>
             {
