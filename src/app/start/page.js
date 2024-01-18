cddbba3e3de0a3fec68acc843ef4cb08e57eaabb
Mukesh Kumar <83938329+MsRajputana6688@users.noreply.github.com>
2024-01-18 18:36:01 +0000

    update page

--- src/app/start/page.js
+++ src/app/start/page.js
@@ -81,7 +81,7 @@ const Home = () => {
     }
 
     return (
-        !result ? <div className='d-flex justify-content-center' style={{ minHeight: '100vh',paddingTop:'180px', alignItems: 'center' }}>
+        !result ? <div className='d-flex justify-content-center' style={{ minHeight: '100vh',paddingTop:'140px', alignItems: 'center' }}>
             {
                 isLoad && <div className="isLoad">
                     <div className="spinner-border text-light" role="status">
